1d758b5e1ac38df60f15c01b4f6d9a82d2eb894d
rafael2801 <spkiller2801@hotmail.com>
2021-01-18 19:55:41 -0200

    Arrumando flatlist, aba de info das consulta e btn de add

--- frontend/src/pages/Consultas/Componentes/ConsultasMarcadas.js
+++ frontend/src/pages/Consultas/Componentes/ConsultasMarcadas.js
@@ -2,25 +2,77 @@
 import React from 'react'
 import { Text, View, TouchableOpacity, Fea, StatusBar } from 'react-native'
 import { Feather } from '@expo/vector-icons';
+import styles from './styles';
 
 
 
 export default function Marcadas({ route, navigation }) {
     const { nome, id } = route.params;
-    return (
-        <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'flex-start', marginTop: '5%' }}>
-            <StatusBar
-                barStyle='dark-content' />
-            <TouchableOpacity onPress={() => navigation.goBack()} style={{
-                marginLeft: -10,
-                marginBottom: 10,
-                width: 30,
-                height: 40
-            }}
-            ><Feather color={'#bbb'} size={40} name={"chevron-left"} />
-            </TouchableOpacity>
-            <Text style={{ fontSize: 24, fontWeight: 'bold', marginStart: '15%', alignItems: 'center' }}>{JSON.stringify(nome)}</Text>
 
+    const InfoKeys = (props) => <Text style={{fontWeight:'bold'}}>{props.name}</Text>
+
+    const Info = (props) => <Text>{props.info}</Text>
+
+    return (
+        <View style={{ flex: 1 }}>
+            <View style={styles.header}>
+
+                <StatusBar
+                    barStyle='dark-content' />
+
+                <TouchableOpacity onPress={() => navigation.goBack()} style={styles.voltar}
+                ><Feather color={'#bbb'} size={40} name={"chevron-left"} />
+                </TouchableOpacity>
+
+                <View style={styles.headerTexto}>
+                    <Text style={{ fontSize: 28, fontWeight: 'bold', }}>Informações</Text>
+                </View>
+
+            </View>
+            <View style={styles.info}>
+
+                <View style={styles.blocoInfo}>
+                    <InfoKeys name={"Especialidade:  "} />
+                    <Info info={JSON.stringify(nome)} />
+                </View>
+
+                <View style={styles.blocoInfo}>
+                    <InfoKeys name={'Dia Marcado:  '} />
+                    <Info info={'12/12/1111'} />
+                </View>
+
+                <View style={styles.blocoInfo}>
+                    <InfoKeys name={'Hora Prevista:  '} />
+                    <Info info={'12:34'} />
+                </View>
+
+                <View style={styles.blocoInfo}>
+                    <InfoKeys name={'Local:  '} />
+                    <Info info={'Rua MiauMiau, 123 São Paulo-SP'} />
+                </View>
+
+                <View style={styles.blocoInfo}>
+                    <InfoKeys name={'Hospital/Clinica:  '} />
+                    <Info info={'Xesquedele no desque saude'} />
+                </View>
+
+                <View style={styles.blocoInfo}>
+                    <InfoKeys name={'Tipo:  '} />
+                    <Info info={'Consulta '} />
+                </View>
+
+                <View style={styles.blocoInfo}>
+                    <InfoKeys name={'Nome:  '} />
+                    <Info info={'Doutor Rafael'} />
+                </View>
+            </View>
         </View>
     )
 }
+/*
+<View style={styles.blocoInfo}>
+</View>
+
+<InfoKeys name={''}/>
+<Info info={''}/>
+  */
--- frontend/src/pages/Consultas/index.js
+++ frontend/src/pages/Consultas/index.js
@@ -1,42 +1,46 @@
 import React, { useState } from 'react';
 import { Feather } from '@expo/vector-icons';
-import { View, Text, TouchableOpacity, Alert, TouchableHighlight, Button, StatusBar } from 'react-native';
+import { View, Text, TouchableOpacity, Alert, TouchableHighlight, Button, StatusBar, FlatList,Image } from 'react-native';
 import styles from './styles';
 import gstyles from '../../gstyles';
-import Consultass from "../Consultas/Dados";
 import { useNavigation } from '@react-navigation/native';
+import consultas from '../Consultas/Dados';
+import Marcadas from './Componentes/Marcadas'
 
-export default function Consultas(){
+
+export default function Consultas() {
 
     const navigation = useNavigation();
 
-    return(
+    return (
         <View style={gstyles.container}>
             <View style={styles.header}>
                 <StatusBar
-                barStyle='dark-content'
-                backgroundColor={'#fafafa'}
+                    barStyle='dark-content'
+                    backgroundColor={'#fafafa'}
                 />
-                <View style={{flexDirection:'row'}}>
-                <TouchableOpacity onPress={() => navigation.goBack()} style={styles.backbutton}><Feather color={'#bbb'} size={40} name={"chevron-left"}/></TouchableOpacity>
-                <Text style={styles.headerTitle}>Consultas Marcadas</Text>
-                </View>
-                <View style={styles.listaConsultasMarcadas}>
-                    {Consultass.map((consulta, index) => (
-                        <View key={consulta.title} style={styles.consultasList}>
-                            <TouchableOpacity
-                            onPress={()=> navigation.navigate('Consultas Marcadas',{nome:consulta.title,id:index})}>
-                                <Text  style={styles.consultasTitle}>{consulta.title}</Text>
-                            </TouchableOpacity>
-                            <TouchableOpacity
-                             onPress={()=>  navigation.navigate('Consultas Marcadas',{nome:consulta.title,id:index}) } >
-                                <Text style={styles.consultasDate}>{consulta.date}</Text>
-                            </TouchableOpacity>
-                            
-                        </View>
-                    ))}
+                <View style={{ flexDirection: 'row' }}>
+                    <TouchableOpacity onPress={() => navigation.goBack()} style={styles.backbutton}><Feather color={'#bbb'} size={40} name={"chevron-left"} /></TouchableOpacity>
+                    <Text style={styles.headerTitle}>Consultas Marcadas</Text>
                 </View>
             </View>
+
+            <View>
+                <FlatList
+                    data={consultas}
+                    keyExtractor={item => item.title}
+                    renderItem={({ item }) => <Marcadas item={item} />} />
+            </View>
+
+            <View style={styles.btnAdd}>
+                <TouchableOpacity
+                onPress={()=>Alert.alert('pera','modal com adiçao de consultas pelo dados.js')}>
+                    <Image
+                    source={require('./img/mais.png')}
+                    resizeMode='contain'/>
+                </TouchableOpacity>
+            </View>
+
         </View>
     );
 }
