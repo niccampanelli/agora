c7f63e09c0a0023ad6b77c1e84c8797c52b0699b
Nicholas Campanelli <nicholasoucampanelli@hotmail.com>
2021-01-25 02:32:13 -0300

    Estilizando mudança de senha

--- frontend/src/gstyles.js
+++ frontend/src/gstyles.js
@@ -1,6 +1,6 @@
 import { StyleSheet } from 'react-native';
 
-const mainAppColor = '#fff000';
+const mainAppColor = '#ffde00';
 const mainTextColor = '#555';
 
 const gstyles = StyleSheet.create({
@@ -21,23 +21,50 @@ const gstyles = StyleSheet.create({
         color: '#555'
     },
 
-    sheetTitle: {
+    bottomSheetBackground: {
+        position: 'absolute',
+        backgroundColor: '#00000055',
+        width: '100%',
+        height: '110%',
+    },
+
+    bottomSheet: {
+        position: 'absolute',
+        bottom: 0,
+        borderTopLeftRadius: 30,
+        borderTopRightRadius: 30,
+        paddingTop: 20,
+        paddingBottom: 100,
+        backgroundColor: '#fff'
+    },
+
+    bottomSheetInfo: {
+        paddingHorizontal: 25
+    },
+    
+    bottomSheetTitle: {
         fontSize: 25,
         fontWeight: 'bold',
         color: '#555',
         marginBottom: 10
     },
 
-    sheetBottomOptions: {
+    bottomSheetPage: {
+        paddingTop: 30,
+        paddingHorizontal: 20,
+    },
+
+    bottomSheetOptions: {
         position: 'absolute',
-        bottom: 70,
+        bottom: 0,
+        height: 50,
         flexDirection: 'row',
         justifyContent: 'space-around',
         width: '100%'
     },
 
-    sheetBottomOptionsTxt: {
-        color: '#0af',
+    bottomSheetOptionsTxt: {
+        color: mainAppColor,
         fontSize: 20
     },
 });
--- frontend/src/pages/Profile/SettingScreen/index.js
+++ frontend/src/pages/Profile/SettingScreen/index.js
@@ -1,16 +1,18 @@
 import React, { useRef, useState } from 'react';
 import { Feather } from '@expo/vector-icons';
-import { View, Text, TouchableOpacity, TextInput, ScrollView } from 'react-native';
+import { View, Text, TouchableOpacity, TextInput, ScrollView, Modal } from 'react-native';
 import styles from './styles';
 import gstyles from '../../../gstyles';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import ConfigButton from '../Components/ConfigButton';
 import ConfigTextButton from '../Components/ConfigTextButton';
-import BottomSheet from '../../Components/BottomSheet';
 
 export default function Home(){
 
     const [isEditingInfo, setInfoEditing] = useState(false);
+
+    const [isModalVisible, setModalVisible] = useState(false);
+    const [modalPage, setModalPage] = useState();
 
     const [emailValue, setEmail] = useState('nicholascampanelli@outlook.com');
     const [passValue, setPass] = useState('************');
@@ -18,9 +20,6 @@ export default function Home(){
     const emailRef = useRef();
     const passRef = useRef();
 
-    const [isBottomSheetEnabled, setBottomSheetEnabled] = useState(false);
-    const [bottomSheetTitle, setBottomSheetTitle] = useState('');
-    const [bottomSheetPage, setBottomSheetPage] = useState('');
     const route = useRoute();
     const navigation = useNavigation();
 
@@ -37,47 +36,9 @@ export default function Home(){
         <ScrollView style={styles.subContainer}>
             <ConfigTextButton iconName={'user'} name={'Nome'} desc={'Nicholas Campanelli de Souza'} destination={''} args={''}/>
             <ConfigTextButton iconName={'credit-card'} name={'CPF'} desc={'268.457.984-45'} destination={''} args={''}/>
-            { isEditingInfo ?
-                <View>
-                    <TouchableOpacity style={styles.listInputButton} onPress={() => {emailRef.current.focus()}}>
-                        <View style={styles.listButtonIcon}>
-                            <Feather size={24} name={'mail'}/>
-                        </View>
-                        <View style={styles.listButtonTxt}>
-                            <Text style={styles.listButtonTitle}>Novo Email</Text>
-                                <TextInput 
-                                    ref={emailRef}
-                                    style={styles.listButtonInput}
-                                    autoCompleteType={'email'}
-                                    value={emailValue}
-                                    onChange={e => {setEmail(e.target.value)}}
-                                />
-                        </View>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.listInputButton} onPress={() => {passRef.current.focus()}}>
-                        <View style={styles.listButtonIcon}>
-                            <Feather size={24} name={'key'}/>
-                        </View>
-                        <View style={styles.listButtonTxt}>
-                            <Text style={styles.listButtonTitle}>Nova Senha</Text>
-                                <TextInput 
-                                    ref={passRef}
-                                    style={styles.listButtonInput}
-                                    autoCompleteType={'password'}
-                                    secureTextEntry
-                                    value={passValue}
-                                    onChange={e => {setPass(e.target.value)}}
-                                />
-                        </View>
-                    </TouchableOpacity>
-                </View>
-            :
-                <View>
-                    <ConfigTextButton iconName={'mail'} name={'Endereço de Email'} desc={'nicholascampanelli@outlook.com'} destination={''} args={''}/>
-                    <ConfigTextButton iconName={'key'} name={'Senha'} desc={'************'} destination={''} args={''}/>
-                </View>
-            }
-            <TouchableOpacity style={styles.listButtonRed} onPress={() => showBottomSheet('Tem certeza?', 'confDelete')}>
+            <ConfigTextButton iconName={'mail'} name={'Endereço de Email'} desc={'nicholascampanelli@outlook.com'} destination={''} args={''}/>
+            <ConfigTextButton iconName={'key'} name={'Senha'} desc={'************'} destination={''} args={''}/>
+            <TouchableOpacity style={styles.listButtonRed} onPress={() => {setModalVisible(true); setModalPage('delete')}}>
                 <View style={styles.listButtonIconRed}>
                     <Feather size={24} name={'trash-2'} color={'#fff'}/>
                 </View>
@@ -100,18 +61,72 @@ export default function Home(){
             <ConfigTextButton iconName={'package'} name={'Versão do Aplicativo'} desc={'5.12.38'} destination={''} args={''}/>
             <ConfigTextButton iconName={'users'} name={'Criado por'} desc={'Nicholas Campanelli de Souza, Rafael da Silva Rodrigues'} destination={''} args={''}/>
         </View>
-    ); 
+    );
 
-    const Delete = () => (
-        <View style={{height: '100%'}}>
-            <Text style={styles.listButtonDesc}>A exclusão do cadastro na plataforma "AGORA" apaga todos os dados do usuário no banco de dados.</Text>
-            <Text style={styles.listButtonDesc}>Esta exclusão é permanente e não pode ser desfeita.</Text>
-            <View style={gstyles.sheetBottomOptions}>
-                <TouchableOpacity onPress={() => setBottomSheetEnabled(false)}>
-                    <Text style={gstyles.sheetBottomOptionsTxt}>Cancelar</Text>
+    const DataChange = () => (
+        <View style={gstyles.bottomSheet}>
+            <View style={gstyles.bottomSheetInfo}>
+                <Text style={gstyles.bottomSheetTitle}>Atualizar Dados</Text>
+                <Text style={styles.listButtonDesc}>Aqui você pode atualizar seu endereço de email e sua senha.</Text>
+            </View>
+            <View style={gstyles.bottomSheetPage}>
+                <TouchableOpacity style={styles.listInputButton} onPress={() => {emailRef.current.focus()}}>
+                    <View style={styles.listButtonIcon}>
+                        <Feather size={24} name={'mail'}/>
+                    </View>
+                    <View style={styles.listButtonTxt}>
+                        <Text style={styles.listButtonTitle}>Novo Email</Text>
+                            <TextInput 
+                                ref={emailRef}
+                                style={styles.listButtonInput}
+                                autoCompleteType={'email'}
+                                value={emailValue}
+                                onChange={e => {setEmail(e.target.value)}}
+                            />
+                    </View>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.listInputButton} onPress={() => {passRef.current.focus()}}>
+                    <View style={styles.listButtonIcon}>
+                        <Feather size={24} name={'key'}/>
+                    </View>
+                    <View style={styles.listButtonTxt}>
+                        <Text style={styles.listButtonTitle}>Nova Senha</Text>
+                            <TextInput 
+                                ref={passRef}
+                                style={styles.listButtonInput}
+                                autoCompleteType={'password'}
+                                secureTextEntry
+                                value={passValue}
+                                onChange={e => {setPass(e.target.value)}}
+                            />
+                    </View>
+                </TouchableOpacity>
+                <Text style={styles.listButtonDesc}>Por motivos de segurança, para confirmar a atualização, será enviado um email no endereço indicado.</Text>
+            </View>
+            <View style={gstyles.bottomSheetOptions}>
+                <TouchableOpacity onPress={() => setModalVisible(false)}>
+                    <Text style={gstyles.bottomSheetOptionsTxt}>Cancelar</Text>
                 </TouchableOpacity>
                 <TouchableOpacity>
-                    <Text style={gstyles.sheetBottomOptionsTxt}>Excluir Dados</Text>
+                    <Text style={gstyles.bottomSheetOptionsTxt}>Confirmar</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+
+    const Delete = () => (
+        <View style={gstyles.bottomSheet}>
+            <View style={gstyles.bottomSheetInfo}>
+                <Text style={gstyles.bottomSheetTitle}>Tem Certeza?</Text>
+                <Text style={styles.listButtonDesc}>A exclusão do cadastro na plataforma "AGORA" apaga todos os dados do usuário no banco de dados.</Text>
+                <Text style={styles.listButtonDesc}>Esta exclusão é permanente e não pode ser desfeita.</Text>
+            </View>
+            <View style={gstyles.bottomSheetOptions}>
+                <TouchableOpacity onPress={() => setModalVisible(false)}>
+                    <Text style={gstyles.bottomSheetOptionsTxt}>Cancelar</Text>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Text style={gstyles.bottomSheetOptionsTxt}>Confirmar</Text>
                 </TouchableOpacity>
             </View>
         </View>
@@ -121,7 +136,8 @@ export default function Home(){
         conta: <Conta/>,
         privacidade: <Privacidade/>,
         sobre: <Sobre/>,
-        confDelete: <Delete/>
+        datachange: <DataChange/>,
+        delete: <Delete/>
     };
 
     return(
@@ -129,17 +145,17 @@ export default function Home(){
         <TouchableOpacity onPress={() => navigation.goBack()} style={styles.backbutton}><Feather color={'#bbb'} size={40} name={"chevron-left"}/></TouchableOpacity>
             <View style={styles.header}>
                 <Text style={styles.headerTitle}>{title}</Text>
-                <TouchableOpacity style={styles.infoEdit} onPress={() => setInfoEditing(!isEditingInfo)}>
+                <TouchableOpacity style={styles.infoEdit} onPress={() => {setModalVisible(true); setModalPage('datachange')}}>
                     {isEditingInfo ? <Feather name={'x'} color={'#888'} size={30}/> : <Feather name={'edit'} color={'#888'} size={30}/>}
                 </TouchableOpacity>
             </View>
             {pages[pageName]}
-            {isBottomSheetEnabled ? 
-                <BottomSheet>
-                    <Text style={gstyles.sheetTitle}>{bottomSheetTitle}</Text>
-                    {pages[bottomSheetPage]}
-                </BottomSheet>
-            : <Text/>}
+            
+            {isModalVisible ? <View style={gstyles.bottomSheetBackground}/> : <Text/>}
+            <Modal transparent animationType={'slide'} visible={isModalVisible}>
+                {pages[modalPage]}
+            </Modal>
+
         </View>
     );
 }
