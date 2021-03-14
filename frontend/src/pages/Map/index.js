e736af62485e9c5b738029af381fd2948eba894c
Nicholas Campanelli <nicholasoucampanelli@hotmail.com>
2021-03-14 18:47:59 -0300

    Corrigindo erro do WebView

--- frontend/src/pages/Map/index.js
+++ frontend/src/pages/Map/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { WebView } from 'react-native-webview';
 import { View, Text, TouchableOpacity, ScrollView , StatusBar} from 'react-native';
 import gstyles, {mainAppColor, mainTextColor, lightTextColor} from '../../gstyles';
 
@@ -7,10 +6,6 @@ export default function Map(){
 
     return(
     <View style={gstyles.container}>
-        <WebView
-            originWhitelist={['*']}
-            source={{ uri: 'https://expo.io' }}
-        />
     </View>
     );
 }
