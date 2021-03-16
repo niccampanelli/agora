d4f861d4da17d08041c93b22035114b8ee5d2528
Nicholas Campanelli <nicholasoucampanelli@hotmail.com>
2021-03-15 23:55:37 -0300

    Configuração do Mapa no Webview

--- frontend/src/pages/Map/index.js
+++ frontend/src/pages/Map/index.js
@@ -1,13 +1,65 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import * as Location from 'expo-location';
 import { WebView } from 'react-native-webview';
-import { View, Text, TouchableOpacity, ScrollView , StatusBar} from 'react-native';
+import { View, Text, TouchableOpacity, ScrollView , StatusBar, Alert} from 'react-native';
+import styles from './styles';
 import gstyles, {mainAppColor, mainTextColor, lightTextColor} from '../../gstyles';
 
 export default function Map(){
 
+    const [html, setHTML] = useState();
+
+    useEffect(() => {
+        (async () => {
+          let { status } = await Location.requestPermissionsAsync();
+          if (status !== 'granted') {
+            Alert.alert('Permission to access location was denied');
+            return;
+          }
+    
+          let location = await Location.getCurrentPositionAsync({});
+
+          const MapHTML = 
+                `
+                <!DOCTYPE html>
+                <html lang="pt-br">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <script src='https://api.mapbox.com/mapbox-gl-js/v2.1.1/mapbox-gl.js'></script>
+                    <link href='https://api.mapbox.com/mapbox-gl-js/v2.1.1/mapbox-gl.css' rel='stylesheet' />
+                    <link href='./styles.css' rel='stylesheet' />
+                    <title>Mapa de Unidades</title>
+                </head>
+                <body style='padding: 0; margin: 0;'>
+                    <div id='map' style='width: 100%; height: 98vh; padding: 0; margin: 0;'></div>
+                    <script>
+                        mapboxgl.accessToken = 'pk.eyJ1IjoiY2FtcGFuZWxsaW5pY2MiLCJhIjoiY2ttOGR0MnZ5MTZ2MzJvcXM4cG1zczZvNCJ9.T7DbMzvz1uFk5TGQeYxh0A';
+                        var map = new mapboxgl.Map({
+                            container: 'map',
+                            style: 'mapbox://styles/mapbox/light-v10',
+                            center: [${location['coords'].longitude}, ${location['coords'].latitude}],
+                            zoom: 13,
+                        });
+
+                        var marker = new mapboxgl.Marker()
+                            .setLngLat([${location['coords'].longitude}, ${location['coords'].latitude}])
+                            .addTo(map);
+                    </script>
+                </body>
+                </html>
+                `
+          setHTML(MapHTML);
+        })();
+      }, []);
+
     return(
-    <View style={gstyles.container}>
-        <WebView source={{ uri: "https://github.com/" }}/>
+    <View style={styles.container}>
+        <WebView
+            style={styles.webView}
+            originWhitelist={'*'} 
+            source={{ html:  html }}
+        />
     </View>
     );
 }
---
+++ frontend/src/pages/Map/styles.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+
+import {layer0Color,mainTextColor,mainAppColor} from '../../gstyles'
+
+
+const styles = StyleSheet.create({
+
+    container: {
+        padding: 0,
+        position: 'absolute',
+        width: '100%',
+        height: '100%'
+    },
+
+    webView: {
+        padding: 0
+    }
+
+})
+
+export default styles
