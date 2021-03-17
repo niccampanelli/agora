17a5fc0b2f98fa6c11a821b2fc866f520c040478
Nicholas Campanelli <nicholasoucampanelli@hotmail.com>
2021-03-17 04:50:46 -0300

    Compatibilidade com o Heroku

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const routes = express.Router();
 const userController = require('../controllers/userController');
-
+routes.get('/',(req,res)=>{res.send("ta funfando")})
 routes.post('/cadastrar', userController.cadastrar);
 routes.post('/login', userController.logar);
 routes.post('/observador', userController.observador);
+
+routes.get('/pegarInfo',userController.pegarDadosUser)
 
 module.exports = routes;
